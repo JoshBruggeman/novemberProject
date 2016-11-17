b2a71e4be50cdab85c70f15fd9e42e6776781805
Ara Kim <ahrakim44@gmail.com>
2016-11-17 16:00:14 -0600

    table made it better

--- js/dataApp.js
+++ js/dataApp.js
@@ -16,9 +16,13 @@ $('#findMovie').on('click', function() {
         } else {
             // $('#movieInfo').html("<p> " + movieData.Plot + " <i>another word</i></p>");
             $('#moviePoster').empty();
+            //empties the info inside of the table so it does not add to the previous information.
             $('.info').empty();
+            // adds movie poster
             $('#moviePoster').html(' <img class="img-responsive img-border img-left" src=' + movieData.Poster + ' alt="">');
-            $("#tbody").html("<tr><td>" + movieData.Title + "</td><td>" + movieData.Genre + "</td><td>" + movieData.Rated + "</td><td>" + movieData.Director + "</td><td>" + movieData.Actors + "</td><td>" + movieData.Runtime + "</td><td>" + movieData.Plot + "</td><td>");
+
+            $("#tbInfo").html("<tr><th>Title</th><td>"+movieData.Title+"</td></tr><tr><th>Genre</th><td>"+movieData.Genre+"</td></tr><tr><th>Rated</th><td>"+movieData.Rated+"</td></tr><tr><th>Director</th><td>"+movieData.Director+"</td></tr><tr><th>Actors</th><td>"+movieData.Actors+"</td></tr><tr><th>Runtime</th><td>"+movieData.Runtime+"</td></tr><tr><th>Plot</th><td>"+movieData.Plot+"</th></tr>");
+
 
         }
 
