efb1f6c50f8c79d7f9cca565657d719334b82772
Mark Gildea <gildeamark1@gmail.com>
2016-11-28 13:33:55 -0600

    input validation and year next to movie title

--- js/dataApp2.js
+++ js/dataApp2.js
@@ -98,7 +98,7 @@ var processYoutube = function(youtube){
     //This prints the id of youtube video.
     console.log(videoId);
 
-    $('#youtube').html('<iframe width="206" height="280" src="https://www.youtube.com/embed/'+videoId+'" frameborder="0" allowfullscreen></iframe>');
+    $('#youtube').html('<iframe width="100%" height="280" src="https://www.youtube.com/embed/'+videoId+'" frameborder="0" allowfullscreen></iframe>');
 
 };
 
@@ -129,9 +129,40 @@ var processMovieData = function(movieData) {
             // adds movie poster
             $('#moviePoster').html(' <img class="img-responsive img-border img-left" src=' + movieData.Poster + ' alt="">');
 
-            $("#tbInfo").html("<tr><th>Title</th><td>"+movieData.Title+"</td></tr><tr><th>Genre</th><td>"+movieData.Genre+"</td></tr><tr><th>Awards</th><td>"+movieData.Awards+"</td></tr><tr><th>Rated</th><td>"+movieData.Rated+"</td></tr><tr><th>Director</th><td>"+movieData.Director+"</td></tr><tr><th>Casts</th><td>"+movieData.Actors+"</td></tr><tr><th>Runtime</th><td>"+movieData.Runtime+"</td></tr><tr><th>Plot</th><td>"+movieData.Plot+"</th></tr>");
+            $("#tbInfo").html("<tr><th>Title</th><td>"+movieData.Title+" ("+(movieData.Year)+")</td></tr><tr><th>Genre</th><td>"+movieData.Genre+"</td></tr><tr><th>Awards</th><td>"+movieData.Awards+"</td></tr><tr><th>Rated</th><td>"+movieData.Rated+"</td></tr><tr><th>Director</th><td>"+movieData.Director+"</td></tr><tr><th>Casts</th><td>"+movieData.Actors+"</td></tr><tr><th>Runtime</th><td>"+movieData.Runtime+"</td></tr><tr><th>Plot</th><td>"+movieData.Plot+"</th></tr>");
 
 
         }
 
     };
+
+// User Input Validation
+// $(document).ready(function() {
+
+// $('#movie-input').on('input', function() {
+//   var input=$(this);
+//   var is_name=input.val();
+//   if(is_name){input.removeClass("invalid").addClass("valid");}
+//   else{input.removeClass("valid").addClass("invalid");}
+// });
+
+// // After Form Submitted Validation
+// $("#findMovie button").click(function(event){
+//   var form_data=$("#contact").serializeArray();
+//   var error_free=true;
+//   for (var input in form_data){
+//     var element=$("#contact" + form_data[input]['name']);
+//     var valid=element.hasClass("valid");
+//     var error_element=$("span", element.parent());
+//     if (!valid){error_element.removeClass("error").addClass("error_show"); error_free=false;}
+//     else{error_element.removeClass("error_show").addClass("error");}
+//   }
+//   if (!error_free){
+//     event.preventDefault(); 
+//   }
+//   else{
+//     alert('No errors: Form will be submitted');
+//   }
+// });
+
+// });
